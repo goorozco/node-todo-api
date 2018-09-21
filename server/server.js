de28188ef3027804a1ed268bd54e374eb4af59fa
Gonzalo Orozco <gonzalo.gorozco@mercadolibre.com>
2018-09-21 11:47:53 -0300

    get

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-let {mongoose} = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 let app = express();
 
@@ -15,16 +16,31 @@ app.post('/todos', (req, res) => {
 	});
 	todo.save().then((doc) => {
 		res.send(doc);
-	}, (err) => {
-		res.status(400).send(err);
+	}).catch( (e) => {
+		res.status(400).send();
 	});
 });
 
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		res.send({todos});
-	}, (err) => {
-		res.status(400).send(err);
+	}).catch( (e) => {
+		res.status(400).send();
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	let id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch( (e) => {
+		res.status(400).send();
 	});
 });
 
