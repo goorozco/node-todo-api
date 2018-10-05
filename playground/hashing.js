4a4aecf0f0b2fbc9f4e9a1dbaed96d74cebe86a2
Gonzalo Orozco <gonzalo.gorozco@mercadolibre.com>
2018-10-04 21:21:40 -0300

    psw salt

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var psw = 'dumy.123';
 
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(psw, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
 
-var decoded= jwt.verify(token, '123abc');
-console.log(decoded);
+
+var hashedpsw = '$2a$10$f0eXEYBJNFuvuvFnGSR58eQ8CIQj.L23u/dqTQKxM5vAFuHOg6A7W';
+bcrypt.compare(psw, hashedpsw, (err, res) => {
+	console.log(res);
+});
+// var data = {
+// 	id: 10
+// };
+
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded= jwt.verify(token, '123abc');
+// console.log(decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 
